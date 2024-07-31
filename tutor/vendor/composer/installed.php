005c850fcc62decac3ed02bd2e8ebb83a03286a3
Justin Gardner <justin@rhynorater.com>
2024-07-31 06:03:55 -0500

    2.7.4

--- tutor/vendor/composer/installed.php
+++ tutor/vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'themeum/tutor',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
+        'reference' => '2ab278a6434d0ea84a098483cb3c39d31ae89a9e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '693b45c174fcd1346ffe588354a672c989d097eb',
-        'name' => 'themeum/tutor',
         'dev' => false,
     ),
     'versions' => array(
         'themeum/tutor' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
+            'reference' => '2ab278a6434d0ea84a098483cb3c39d31ae89a9e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '693b45c174fcd1346ffe588354a672c989d097eb',
             'dev_requirement' => false,
         ),
     ),
